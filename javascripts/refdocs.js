674896d5e17687d2f0bf45ba2edb843a0c335189
desneo <desneo@163.com>
2016-09-02 16:37:22 +0800

    markdown,js

--- javascripts/refdocs.js
+++ javascripts/refdocs.js
@@ -1,5 +1,6 @@
 $( "#refJs" ).on( "click", function( event ) {
-  $.get("./frontend/js/js.md", function(data) { 
-    $(".jumbotron").html( data )
+  $.get("./frontend/js/js.md", function(data) {
+    var html_content = markdown.toHTML( data );
+    $(".jumbotron").html( html_content )
   });
 });
