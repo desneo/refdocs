29c5ae8ef552dfede881746ed82b48743f5f1064
desneo <desneo@163.com>
2016-09-02 16:48:38 +0800

    Update refdocs.js

--- javascripts/refdocs.js
+++ javascripts/refdocs.js
@@ -1,6 +1,6 @@
 $( "#refJs" ).on( "click", function( event ) {
   $.get("./frontend/js/js.md", function(data) {
     var html_content = markdown.toHTML( data );
-    $(".jumbotron").html( html_content )
+    $(".content").html( html_content )
   });
 });
