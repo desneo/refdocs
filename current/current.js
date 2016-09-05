f2072e38c854f1474f5fa79151070a2466c7588b
desneo <desneo@163.com>
2016-09-05 23:49:02 +0800

    Update current.js

--- current/current.js
+++ current/current.js
@@ -2,7 +2,17 @@ function dealmd(path) {
     debugger;
     $.get(path, function(data) {
         var html_content = markdown.toHTML(data);
-        $(".markdown-body").html(html_content)
+        $(".markdown-body").html(html_content);
+        //html自动编号
+        $("h1").each(function(i, vali) {
+        	 $(this).prepend(i+1+'.');
+        	 $("h2").each(function(j, valj){
+        	 	$(this).prepend(i+"."+(j+1)+'.');
+        	 	$("h3").each(function(k, valk){
+        	 		$(this).prepend(i+"."+(j+1)+'.'+(k+1)+'.');
+        	 	});
+        	 });
+    	});
     });
 }
 
