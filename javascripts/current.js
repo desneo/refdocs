6876ae16979e5ebd9e03c72dda82387aef62e242
desneo <desneo@163.com>
2016-09-05 16:28:27 +0800

    update

--- javascripts/current.js
+++ javascripts/current.js
@@ -1,6 +1,13 @@
-$( "#refJs" ).on( "click", function( event ) {
-  $.get("./frontend/js/js.md", function(data) {
-    var html_content = markdown.toHTML( data );
-    $(".markdown-body").html( html_content )
-  });
+function dealmd(path) {
+    debugger;
+    $.get("path", function(data) {
+        var html_content = markdown.toHTML(data);
+        $(".markdown-body").html(html_content)
+    });
+}
+
+$("#js").on("click", function(event) {
+    var value = event.target.text;
+    var path = "./frontend/js/" + value + ".md";
+    dealmd(path);
 });
