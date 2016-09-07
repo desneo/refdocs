850469278e3e49c7a693bfccb3879ed4785b9c88
desneo <desneo@163.com>
2016-09-07 09:22:00 +0800

    Update current.js

--- current/current.js
+++ current/current.js
@@ -4,14 +4,25 @@ function dealmd(path) {
         var html_content = markdown.toHTML(data);
         $(".markdown-body").html(html_content);
         //html自动编号
-        $("h1").each(function(i, vali) {
-        	 $(vali).prepend(i+1+'.');
-        	 $(vali).find("h2").each(function(j, valj){
-        	 	$(valj).prepend(i+"."+(j+1)+'.');
-        	 	$(valj).find("h3").each(function(k, valk){
-        	 		$(valk).prepend(i+"."+(j+1)+'.'+(k+1)+'.');
-        	 	});
-        	 });
+        var h1Index = 0;
+        $("h1,h2,h3,h4").each(function(i, val) {
+        	if("h1" == val.tagName){
+        		h1Index += 1;
+		        var h2Index = 0;
+        	}
+        	$(val).prepend(h1Index+".");
+
+        	if("h2" == val.tagName){
+        		h2Index += 1;
+		        var h3Index = 0;
+        	}
+        	$(val).prepend(h1Index+"." + h2Index + ".");
+
+        	if("h3" == val.tagName){
+        		h3Index += 1;
+		        var h4Index = 0;
+        	}
+        	$(val).prepend(h1Index+"." + h2Index + "." + h3Index + ".");
         });
     });
 }
