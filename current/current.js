73518ee287f96d5b4dc7a02ee4c62a5438ed2af8
desneo <desneo@163.com>
2016-09-07 09:32:08 +0800

    Update current.js

--- current/current.js
+++ current/current.js
@@ -6,23 +6,23 @@ function dealmd(path) {
         //html自动编号
         var h1Index = 0;
         $("h1,h2,h3,h4").each(function(i, val) {
-        	if("h1" == val.tagName){
+        	if("H1" == val.tagName){
         		h1Index += 1;
 		        var h2Index = 0;
+        		$(val).prepend(h1Index+".");
         	}
-        	$(val).prepend(h1Index+".");
 
-        	if("h2" == val.tagName){
+        	if("H2" == val.tagName){
         		h2Index += 1;
 		        var h3Index = 0;
+        		$(val).prepend(h1Index+"." + h2Index + ".");
         	}
-        	$(val).prepend(h1Index+"." + h2Index + ".");
 
-        	if("h3" == val.tagName){
+        	if("H3" == val.tagName){
         		h3Index += 1;
 		        var h4Index = 0;
+        		$(val).prepend(h1Index+"." + h2Index + "." + h3Index + ".");
         	}
-        	$(val).prepend(h1Index+"." + h2Index + "." + h3Index + ".");
         });
     });
 }
